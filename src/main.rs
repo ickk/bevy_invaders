d44aea1b863b4508d8c372084d4d22bddef53a84
ickk <git@ickk.io>
2021-06-12 16:21:25 +1000

    change the background color render a texture to the screen

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,19 @@ fn main() {
 fn setup(
   mut commands: Commands,
   asset_server: Res<AssetServer>,
-  mut materials: ResMut<Assets<ColorMaterial>>,
+  mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
   commands.insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)));
+  commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+  let texture_handle = asset_server.load("ferris.png");
+  let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(26.0, 26.0), 2, 3);
+  let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
   commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+  commands.spawn_bundle(SpriteSheetBundle {
+    texture_atlas: texture_atlas_handle,
+    transform: Transform::from_scale(Vec3::splat(2.0)),
+    ..Default::default()
+  }).insert(Timer::from_seconds(0.1, true));
 }
