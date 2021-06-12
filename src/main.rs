f0eebef27a91e83c519821755820ab8f5811ae90
ickk <git@ickk.io>
2021-06-13 03:09:22 +1000

    wip projectile system

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::{prelude::*,
            sprite};
+use std::collections::HashMap;
 
 fn print_build_metadata() {
   if let Some(build_date) = option_env!("BUILD_DATE") {
@@ -15,34 +16,68 @@ fn main() {
 
   App::build()
     .add_plugins(DefaultPlugins)
+    .insert_resource(HandleMap::new())
     .add_startup_system(setup.system())
     .add_system(movement_system.system())
     .add_system(player_sprite_system.system())
+    .add_system(player_shoot_system.system())
+    .add_system(projectile_move_system.system())
+    .add_event::<ShootEvent>()
     .run();
+}
+
+struct HandleMap {
+  textures: HashMap<&'static str, Handle<Texture>>,
+  texture_atlases: HashMap<&'static str, Handle<TextureAtlas>>,
+  color_materials: HashMap<&'static str, Handle<ColorMaterial>>,
+}
+impl HandleMap {
+  fn new() -> Self {
+    HandleMap {
+      textures: HashMap::new(),
+      texture_atlases: HashMap::new(),
+      color_materials: HashMap::new(),
+    }
+  }
 }
 
 fn setup(
   mut commands: Commands,
   asset_server: Res<AssetServer>,
   mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+  mut materials: ResMut<Assets<ColorMaterial>>,
+  mut handles: ResMut<HandleMap>,
 ) {
-  commands.insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)));
-  commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+  // Get handles for assets
+  handles.textures
+    .insert("player", asset_server.load("ferris.png"));
+  handles.textures
+    .insert("projectile", asset_server.load("projectile.png"));
+  let projectile_material_handle = materials.add(
+    handles.textures.get("projectile").unwrap().clone().into()
+  );
+  handles.color_materials
+    .insert("projectile", projectile_material_handle);
 
-  let texture_handle = asset_server.load("ferris.png");
-  let mut texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(29.0, 21.0), 6, 1);
+  let mut texture_atlas = TextureAtlas::from_grid(
+    handles.textures.get("player").unwrap().clone(),
+    Vec2::new(29.0, 21.0), 6, 1);
   texture_atlas.add_texture(sprite::Rect {
     min: Vec2::new(0.0, 0.0),
     max: Vec2::new(29.0, 21.0)
   });
-  let texture_atlas_handle = texture_atlases.add(texture_atlas);
+  handles.texture_atlases
+    .insert("player", texture_atlases.add(texture_atlas));
+
+  // Make background black
+  commands.insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)));
 
   // spawn camera
   commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
   // Spawn player
   commands.spawn_bundle(SpriteSheetBundle {
-    texture_atlas: texture_atlas_handle,
+    texture_atlas: handles.texture_atlases.get("player").unwrap().clone(),
     transform: {
         let mut trans = Transform::from_scale(Vec3::splat(2.0));
         trans.translation.y -= 325.0;
@@ -63,28 +98,36 @@ fn player_sprite_system(
   mut query: Query<(&mut TextureAtlasSprite,
                     &Velocity,
                     &Player)>,
+  mut shootevent: EventReader<ShootEvent>,
 ) {
   let (mut sprite, velocity, player) = query.single_mut()
     .expect("There was more than one player entity");
-  match (&player.facing, velocity.x.abs()) {
-    (Direction::Left, speed) if speed > 0.0 => sprite.index = 0,
-    (Direction::Right, speed) if speed > 0.0 => sprite.index = 1,
-    (Direction::Left, _) => sprite.index = 4,
-    (Direction::Right, _) => sprite.index = 5,
+  let mut event = None;
+  for e in shootevent.iter() {
+    event = Some(e);
+  }
+  match (&player.facing, velocity.x.abs(), event) {
+    (Direction::Left, _, Some(e)) => {sprite.index = 0; eprintln!("debug: {:#?}", e)},
+    (Direction::Right, _, Some(_)) => sprite.index = 1,
+    // (Direction::Left, speed, Some(_)) if speed > 0.0 => sprite.index = 0,
+    // (Direction::Right, speed, Some(_)) if speed > 0.0 => sprite.index = 1,
+    (Direction::Left, _, None) => sprite.index = 4,
+    (Direction::Right, _, None) => sprite.index = 5,
     _ => sprite.index = 4,
   }
 }
 
 fn movement_system(
-  mut query: Query<(&mut Transform,
-                    &mut Velocity,
-                    &mut Player)>,
+  mut query: Query<(&mut Player,
+                    &mut Transform,
+                    &mut Velocity)>,
   input: Res<Input<KeyCode>>,
 ) {
-  let (mut transform, mut velocity, mut player) = query.single_mut()
+  let (mut player, mut transform, mut velocity) = query.single_mut()
     .expect("There was more than one player entity");
 
   // update velocity
+  // TODO: use a curve for velocity -> linear accel to some maximum velocity, damper
   velocity.x = (input.pressed(KeyCode::Right) as i32 - input.pressed(KeyCode::Left) as i32) as f32 * 4.0;
   // move player
   transform.translation.x = (-600.0 as f32).max(transform.translation.x + velocity.x).min(600.0);
@@ -106,6 +149,59 @@ fn movement_system(
   };
 }
 
+fn projectile_move_system(
+  mut query: Query<(Entity,
+                    &Velocity,
+                    &mut Transform),
+                   With<Projectile>>,
+  mut commands: Commands,
+) {
+  for (projectile, velocity, mut transform) in query.iter_mut() {
+    // despawn projectiles that go off-screen
+    if transform.translation.y > 360.0 {
+      commands.entity(projectile).despawn();
+      continue
+    }
+    transform.translation.x += velocity.x * 0.5;
+    transform.translation.y += velocity.y;
+  }
+}
+
+// TODO: add timer for shoot sprite
+// TODO: alternate shooting claws
+fn player_shoot_system(
+  mut query: Query<(&Player,
+                    &Velocity,
+                    &Transform)>,
+  mut event: EventWriter<ShootEvent>,
+  mut commands: Commands,
+  handles: Res<HandleMap>,
+  input: Res<Input<KeyCode>>,
+) {
+  let (player, player_velocity, player_transform) = query.single_mut()
+    .expect("There was more than one player entity");
+
+  if input.just_released(KeyCode::Space) {
+    let mut projectile_transform = player_transform.clone();
+    projectile_transform.translation.y += 30.0;
+    match player.facing {
+      Direction::Right  => projectile_transform.translation.x += 30.0,
+      Direction::Left|_ => projectile_transform.translation.x -= 30.0,
+    }
+
+    commands.spawn_bundle(SpriteBundle {
+      material: handles.color_materials.get("projectile").unwrap().clone(),
+      transform: projectile_transform,
+      ..Default::default()
+    }).insert(Velocity {
+      x: player_velocity.x,
+      y: 4.0,
+    }).insert(Projectile);
+
+    event.send(ShootEvent)
+  }
+}
+
 #[derive(Copy, Clone)]
 enum Direction {
   Right,
@@ -113,14 +209,15 @@ enum Direction {
   None,
 }
 
+struct Projectile;
+#[derive(Debug)]
+struct ShootEvent;
+
 struct Player {
   facing: Direction,
 }
 
 struct Velocity {
   x: f32,
-  #[allow(unused)]
   y: f32,
 }
-
-
